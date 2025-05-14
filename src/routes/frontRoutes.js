45f10b2cec9c56e9aab1f46fae2d3265747d19d3
Maria Eduarda <mariaeduardabarbosah@gmail.com>
2025-05-14 15:54:02 -0300

    fix: correção de documentos

--- src/routes/frontRoutes.js
+++ src/routes/frontRoutes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 
-// Roteamento para páginas dinâmicas
 router.get('/', (req, res) => {
   res.render(path.join(__dirname, '../views/layout/main'), {
     pageTitle: 'Página Inicial',
@@ -17,6 +16,5 @@ router.get('/about', (req, res) => {
   });
 });
 
-// Adicione outras rotas conforme necessário
 
 module.exports = router;
